49adc3c079ae0d44f8fac5f2c397da95f30516e8
BuildTools <unconfigured@null.spigotmc.org>
2017-07-12 12:05:50 +0100

    - Changed license header

--- src/main/java/io/github/davidg95/jconn/JConnConnectionAccept.java
+++ src/main/java/io/github/davidg95/jconn/JConnConnectionAccept.java
@@ -1,6 +1,22 @@
-/*
- * JConn networking framewotk.
- * Created by David Grant
+/* 
+ * JConn TCP networking framework.
+ *
+ * Copyright (C) 2017 David A. Grant
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To get in touch with me, send an email to pirakaleader@googlemail.com.
  */
 package io.github.davidg95.jconn;
 
@@ -51,7 +67,7 @@ public class JConnConnectionAccept extends Thread {
 
     private static final List<JConnThread> THREADS = new LinkedList<>();
     private static final StampedLock LOCK = new StampedLock();
-    
+
     /**
      * All the detected method which have the @JConnMethod annotation.
      */
@@ -112,7 +128,7 @@ public class JConnConnectionAccept extends Thread {
     protected static void unlockRead(final long stamp) {
         LOCK.unlockRead(stamp);
     }
-    
+
     /**
      * Scans this class and finds all method with the JConnMethod annotation.
      */
